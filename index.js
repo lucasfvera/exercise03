f3866bef613823e53e84c0d73e731859f1a3f429
lucasfvera <lucasfvera@hotmail.com>
2023-07-23 17:00:47 -0300

    Update remote with local

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const shareLink = document.getElementById("share-icon");
 const shareOptionsContainer = document.getElementById("share-options-container")
 console.log(shareOptionsContainer.classList)
 
-const toggleContainerView = ()=>{
+const toggleContainerView = (e)=>{
     shareOptionsContainer.classList.toggle("hidden")
-    return console.log("clicked")
+    return console.log("clicked", e.target)
 }
 
 shareLink.addEventListener("click",toggleContainerView)
